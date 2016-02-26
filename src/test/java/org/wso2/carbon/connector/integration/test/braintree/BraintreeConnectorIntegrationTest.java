0e9959124b042ccd258064aeaf22a6dbe2231b61
Hariprasath <tharis63@gmail.com>
2016-02-26 17:51:32 +0530

    Added version

--- src/test/java/org/wso2/carbon/connector/integration/test/braintree/BraintreeConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/braintree/BraintreeConnectorIntegrationTest.java
@@ -67,7 +67,7 @@ public class BraintreeConnectorIntegrationTest extends ConnectorIntegrationTestB
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("braintree-connector-1.0.0");
+        init("braintree-connector-1.0.1-SNAPSHOT");
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
 
